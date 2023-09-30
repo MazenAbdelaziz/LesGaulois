42818cd0f8bfd00e257c78829b88c5519b3ec93a
MazenAbdelaziz <145658862+MazenAbdelaziz@users.noreply.github.com>
2023-09-30 16:50:18 +0200

    TP2 Done

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,6 +3,8 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements;
+	private int nbEquipement;
 
 	public Romain(String nom, int force) {
 		this.nom = nom;
@@ -10,6 +12,8 @@ public class Romain {
 			throw new IllegalArgumentException("La force d'un Romain doit être positive.");
 		}
 		this.force = force;
+		this.equipements = new Equipement[2];
+		this.nbEquipement = 0;
 	}
 
 	public String getNom() {
@@ -39,13 +43,71 @@ public class Romain {
 		}
 	}
 
+	public void ajouterEquipement(Equipement equipement) {
+		if (nbEquipement < equipements.length) {
+			equipements[nbEquipement] = equipement;
+			nbEquipement++;
+		} else {
+			System.out.println("Nombre maximum d'équipements atteint.");
+		}
+	}
+
+	public void afficherEquipements() {
+		System.out.print("Équipements de " + nom + ": ");
+		for (int i = 0; i < nbEquipement; i++) {
+			System.out.print(equipements[i].getNom() + " ");
+		}
+		System.out.println();
+	}
+
+	public void sEquiper(Equipement equipement) {
+		switch (nbEquipement) {
+		case 0:
+			ajouterEquipement(equipement);
+			System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement.getNom() + ".");
+			break;
+		case 1:
+			if (possedeEquipement(equipement)) {
+				System.out.println("Le soldat " + nom + " possède déjà un " + equipement.getNom() + " !");
+			} else {
+				ajouterEquipement(equipement);
+				System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement.getNom() + ".");
+			}
+			break;
+		case 2:
+			System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+			break;
+		default:
+			System.out.println("Erreur : nombre d'équipements invalide.");
+		}
+	}
+
+	private boolean possedeEquipement(Equipement equipement) {
+		return equipements[0] == equipement;
+	}
+
 	public static void main(String[] args) {
 		Gaulois asterix = new Gaulois("Astérix", 8);
 		Romain minus = new Romain("Minus", 6);
 		System.out.println(asterix.getNom());
 		System.out.println(asterix);
-//		minus.prendreParole();
-//		minus.parler(null);
-//		minus.recevoirCoup(10);
+
+		// Tests
+		minus.prendreParole();
+		minus.parler(null);
+		minus.recevoirCoup(10);
+
+		// Tester l'énumération Equipement
+		Equipement monEquipement = Equipement.CASQUE;
+		System.out.println("Mon équipement : " + monEquipement);
+		Equipement autreEquipement = Equipement.BOUCLIER;
+		System.out.println("Autre équipement : " + autreEquipement);
+
+		// Tester la méthode sEquiper
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
 	}
+
 }
